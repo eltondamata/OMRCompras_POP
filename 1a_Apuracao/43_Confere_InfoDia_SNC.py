be0e5052b0fa32bc77315408359964b4c5b26bcf
eltondamata <eltondamata@gmail.com>
2022-03-31 10:01:08 -0300

    inclusao consulta 43_Confere_InfoDia_SNC.py

---
+++ 1a_Apuracao/43_Confere_InfoDia_SNC.py
@@ -0,0 +1,42 @@
+﻿#elton.mata@martins.com.br
+'''
+Confere a carga da Meta POP no InfoDia e SNC
+Tabela InfoDia é atualizada todo último dia útil do mês às 18:00
+Tabela SNC é atualizada todo dia 1º
+'''
+
+import pandas as pd
+import sys
+sys.path.insert(0, r'C:\oracle\dwh')
+from OracleMRT import conn
+from datetime import date, timedelta
+pd.options.display.float_format = '{:,.2f}'.format
+
+MESREF = (date.today().replace(day=1) - timedelta(days=0)).strftime("%Y%m") #Mes Atual
+#MESREF = 202203
+
+#Consulta Infodia ['ANOMESREF', 'CODFILEMPEPD', 'VLRTOTFAT' AS 'RLZ', 'VLRPRVFAT' AS 'OMR']
+mysql = (f"""     
+select * from MRT.T0160623 WHERE ANOMESREF = {MESREF} 
+--FETCH FIRST 5 ROWS ONLY
+  """)
+infodia = pd.read_sql(mysql, con=conn)
+print(f'InfoDia: {MESREF}')
+print(pd.DataFrame(infodia.agg({'VLRTOTFAT':'sum', 'VLRPRVFAT': 'sum'}) \
+                          .rename({'VLRTOTFAT':'FAT_RLZ', 'VLRPRVFAT': 'FAT_POP'})) \
+                          .transpose().to_markdown(tablefmt='github', floatfmt=',.2f', index=False), '\n')
+
+#Consulta SNC ['DATREF', 'CODFILEMPEPD', 'CODFRN', 'CODGRPMER', 'CODFMLMER', 'CODCLSMER', 'VLRTOTVNDPPS', 'VLRPPSMRGCRBVND', 'VLRRCTLIQAPUOCD', 'VLRMRGBRTOCD']
+#MRT.HSTDIRPPSFRNCTG frnctg
+#MRT.HSTDIRPPSFRNEST  frnest
+mysql = (f"""     
+select * from MRT.HSTDIRPPSFRNCTG 
+WHERE TO_CHAR(DATREF, 'YYYYMM') = {MESREF}
+--FETCH FIRST 5 ROWS ONLY
+  """)
+df = pd.read_sql(mysql, con=conn)
+conn.close()
+print(f'META SNC: {MESREF}')
+print(pd.DataFrame(df.agg({'VLRTOTVNDPPS':'sum', 'VLRRCTLIQAPUOCD': 'sum', 'VLRMRGBRTOCD': 'sum', 'VLRPPSMRGCRBVND': 'sum'}) \
+                     .rename({'VLRTOTVNDPPS':'FATURAMENTO', 'VLRRCTLIQAPUOCD': 'REC_LIQUIDA', 'VLRMRGBRTOCD': 'MARGEM BRUTA', 'VLRPPSMRGCRBVND': 'MARGEM CONTRIBUICAO'})) \
+                     .transpose().to_markdown(tablefmt='github', floatfmt=',.2f', index=False))
