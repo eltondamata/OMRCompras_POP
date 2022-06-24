0324f05a380d5738ad99fd1ff3a483d309578863
eltondamata <eltondamata@gmail.com>
2022-06-24 10:49:44 -0300

    20220624 Finalizado 22_Opcional_BreakBackOMR_COMPRAS_Final.py

--- 1a_Apuracao/22_Opcional_BreakBackOMR_COMPRAS_Final.py
+++ 1a_Apuracao/22_Opcional_BreakBackOMR_COMPRAS_Final.py
@@ -18,9 +18,7 @@ with open('../Parametros/caminho.txt','r') as f:
 Planilha_Contribuicao = (caminho + 'OMR_CategoriaFornecedorEstadoCanal_CARGA.xlsx')
 omrcmp = pd.read_feather(caminho + 'bd/OMR_COMPRAS_POPAJT.ft')
 
-#Parametros
 valores_calc = ['VLRVNDFATLIQ', 'VLRRCTLIQAPU', 'VLRMRGBRT', 'VLRCSTMC']
-
 df = pd.read_excel(Planilha_Contribuicao, 'VAREJO', skiprows=6, usecols="B:K,O:Q")
 df.insert(0,'DESDRTCLLATU','VAREJO ALIMENTAR E FARMA')
 df1 = pd.read_excel(Planilha_Contribuicao, 'ELETRO', skiprows=6, usecols="B:K,O:Q")
@@ -31,14 +29,22 @@ df1.insert(0,'DESDRTCLLATU','MARTCON/AGROVET')
 df = df.append(df1)
 df = df.query('~FAT.isnull()', engine='python') #eliminar linhas com valores nulos
 df.rename(columns={'CODFRN':'CODDIVFRN', 'Célula':'DESCLLCMPATU', 'Estado':'CODESTUNI', 'Canal':'DESTIPCNLVNDOMR', 'FAT':'VLRVNDFATLIQ', 'RL':'VLRRCTLIQAPU', 'MB':'VLRMRGBRT', 'MC':'VLRMRGCRB'}, inplace=True)
-#colunas df = ['DESDRTCLLATU', 'CATEGORIA', 'CODGRPPRD', 'CODCTGPRD', 'FORNECEDOR', 'CODDIVFRN', 'GRUPO FORNECEDOR', 'DESCLLCMPATU', 'CODESTUNI', 'DESTIPCNLVNDOMR', 'VLRVNDFATLIQ', 'VLRRCTLIQAPU', 'VLRMRGBRT', 'VLRMRGCRB']
 df[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT', 'VLRMRGCRB']] = df[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT', 'VLRMRGCRB']].mul(1000)
 df['VLRCSTMC'] = df['VLRMRGCRB'] - df['VLRMRGBRT']
 df['DESTIPCNLVNDOMR'].replace('ATACADO', 'OUTROS CANAIS', inplace=True)
 
-print('VALOR TOTAL IMPORTADO DO ARQUIVO EXCEL')
-confere = df[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT', 'VLRMRGCRB']].sum().reset_index().transpose()
-print(confere.to_string())
+print('-------------------------------------- IMPUTS --------------------------------------')
+print('\nVALOR IMPORTADO DO ARQUIVO EXCEL -- OMR_CategoriaFornecedorEstadoCanal_CARGA.xlsx') 
+#DIRETORIA
+confere = df.groupby(['DESDRTCLLATU'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT', 'VLRMRGCRB']].sum()
+confere.loc['TOTAL'] = confere.iloc[:,-4:].sum(axis=0)
+confere = confere.reset_index()
+print(confere.to_markdown(index=False, tablefmt='github', floatfmt=',.2f', numalign='right'), '\n')
+#CANAL
+confere = df.groupby(['DESTIPCNLVNDOMR'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT', 'VLRMRGCRB']].sum()
+confere.loc['TOTAL'] = confere.iloc[:,-4:].sum(axis=0)
+confere = confere.reset_index()
+print(confere.to_markdown(index=False, tablefmt='github', floatfmt=',.2f', numalign='right'), '\n')
 
 df = pd.melt(
 	df, id_vars=['DESDRTCLLATU', 'CODGRPPRD', 'CODCTGPRD', 'CODDIVFRN', 'CODESTUNI', 'DESTIPCNLVNDOMR'],
@@ -55,20 +61,12 @@ df_full = pd.melt(
 	value_vars=valores_calc,
 	var_name='MEDIDA',
 	value_name='DRIVER')
-print('\n', "BASE COMPLETA ANTES DO BREAKBACK")
-confere = df_full.pivot_table(index=['DESDRTCLLATU'], columns=['MEDIDA'], values='DRIVER', aggfunc=sum)[valores_calc]
-confere.loc['TOTAL',:] = confere.sum()
-print(confere.to_string())
 
 #Base Completa após Distribuir o valor de DIRETORIA x CATEGORIA x FORN na base completa
 df_full['DRIVER'] = (df_full['DRIVER'] / df_full.groupby(['DESDRTCLLATU', 'CODGRPPRD', 'CODCTGPRD', 'CODDIVFRN', 'CODESTUNI', 'DESTIPCNLVNDOMR','MEDIDA'])['DRIVER'].transform('sum'))
 df_full = df_full.merge(df, how='inner', on=['DESDRTCLLATU', 'CODGRPPRD', 'CODCTGPRD', 'CODDIVFRN', 'CODESTUNI', 'DESTIPCNLVNDOMR','MEDIDA'])
 df_full['DRIVER'] = df_full['DRIVER'] * df_full['VALOR']
 del df_full['VALOR']
-print('\n', "BASE COMPLETA APOS DISTRIBUIR OS VALORES DO ARQUIVO EXCEL")
-confere = df_full.pivot_table(index=['DESDRTCLLATU'], columns=['MEDIDA'], values='DRIVER', aggfunc=sum)[valores_calc]
-confere.loc['TOTAL',:] = confere.sum()
-print(confere.to_string())
 
 #Transpor linhas para colunas no dataset OMR_Compras (base completa)
 df_full = df_full.pivot_table(index=['NOMMES', 'CODGRPPRD', 'CODCTGPRD', 'CODSUBCTGPRD', 'DESCTGPRD', 'CODDIVFRN', 'DESDIVFRN', 'NOMGRPECOFRN', 'DESDRTCLLATU', 'DESCLLCMPATU', 'DESTIPCNLVNDOMR', 'CODESTUNI', 'CODFILEPD'], values='DRIVER', columns=['MEDIDA'], aggfunc=sum).reset_index()
@@ -76,19 +74,29 @@ df_full = df_full.query('VLRVNDFATLIQ>0')
 df_full.eval('VLRMRGCRB=VLRMRGBRT+VLRCSTMC', inplace=True)
 df_full.to_feather(caminho + 'bd/OMR_COMPRAS_Final.ft')
 
+print('-------------------------------------- CONFERENCIA BASES DE CARGA CALCULADA --------------------------------------')
+print('\n', "==> OMR COMPRAS FINAL") 
+#FILIAL
+confere = df_full.groupby(['CODFILEPD'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT','VLRMRGCRB']].sum()
+confere.loc['TOTAL',:] = confere.sum()
+print(confere.to_markdown(tablefmt='github', floatfmt=',.2f', numalign='right'), '\n')
+#DIRETORIA
+confere = df_full.groupby(['DESDRTCLLATU'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT','VLRMRGCRB']].sum()
+confere.loc['TOTAL',:] = confere.sum()
+print(confere.to_markdown(tablefmt='github', floatfmt=',.2f', numalign='right'), '\n')
 #Gera arquivo formato tabela de carga dwh.RLCOMRCMPOCDOPE
 df_full = df_full.groupby(['NOMMES', 'CODGRPPRD', 'CODCTGPRD', 'CODSUBCTGPRD', 'CODDIVFRN', 'CODESTUNI', 'CODFILEPD', 'DESTIPCNLVNDOMR'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT','VLRMRGCRB']].sum().reset_index()
 df_full.rename(columns={'CODDIVFRN':'CODFRN'}, inplace=True)
+#CANAL
+confere = df_full.groupby(['DESTIPCNLVNDOMR'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT','VLRMRGCRB']].sum()
+confere.loc['TOTAL',:] = confere.sum()
+print(confere.to_markdown(tablefmt='github', floatfmt=',.2f', numalign='right'), '\n')
 
-print('\n', "==> BASE CARGA POR TIPO DE CANAL")
-confere3 = df_full.groupby(['DESTIPCNLVNDOMR'])[['VLRVNDFATLIQ','VLRRCTLIQAPU','VLRMRGBRT','VLRMRGCRB']].sum()
-confere3.eval('VLRCSTMC=VLRMRGCRB-VLRMRGBRT', inplace=True)
-confere3.loc['TOTAL',:] = confere3.sum()
-print(confere3.to_markdown(tablefmt='github', floatfmt=',.2f', numalign='right'))
-
-print('\n', "TABELA DE CARGA")
-print(df_full.nlargest(5, 'VLRVNDFATLIQ').to_string(index=False),'\n')
+#Confere os cinco maiores e menores registros da tabela de carga
+print('\n', "TABELA DE CARGA (Cinco Maiores e Menores registros no dataset de carga)")
+print(df_full.nlargest(5, 'VLRVNDFATLIQ').to_string(index=False))
 print(df_full.nsmallest(5, 'VLRVNDFATLIQ').to_string(index=False),'\n')
+print('\n', "==> Describe do dataset de carga (count: 80~90 mil; min: não pode ser negativo para FAT, RL, MB)")
 print(df_full.describe().to_string())
 
 df_full.to_feather(caminho + 'bd/RLCOMRCMPOCDOPE_CARGA.ft')
